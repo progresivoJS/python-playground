18f163d8c6b00f59efd73988ee4666a123a5c02f
progresivojs <progresivojs@gmail.com>
2018-08-03 19:17:13 +0900

    feat: study how to be pythonic in string.

---
+++ dict_comprehension.py
@@ -0,0 +1,3 @@
+# Dictionary Comprehension
+
+user_email = {user.name: user.email for user in users_list if user.email}
---
+++ string.py
@@ -0,0 +1,19 @@
+# use format function like **kwargs
+
+def get_formmated_user_info(user):
+    output = 'Name: {user.name}, Age: {user.age}, Sex: {user.sex}'.format(user=user)
+    
+# join
+# use ''.join() instead of string append
+# It's faster and use less memory.
+
+result_list = ['I', 'am', 'Groot']
+result_string = '-'.join(result_list)
+print(result_string)
+
+# chain string functions.
+# But no more than three chained functions.
+book_info = ' The Three Musketeers: Alexandre Dumas'
+formatted_book_info = book_info.strip().upper().replace(':', ' by')
+print(formatted_book_info)
+
